2a343c81161020136662f4de80d2e210cb8c83a6
manuelAlonsoPerez <manuelalonsoper@gmail.com>
2026-05-14 11:47:05 +0200

    C

--- db/seed_delete_profile_by_id.sql
+++ db/seed_delete_profile_by_id.sql
@@ -1,6 +1,6 @@
 DO $$
 DECLARE
-  pid uuid := 'e1304b85-3d56-42c7-bc25-76d6bc322a9c';
+  pid uuid := '1bd4472a-45d3-4242-a401-397406e049cc';
 BEGIN
   DELETE FROM scenarios          WHERE profile_id = pid;
   DELETE FROM document_analyses  WHERE profile_id = pid;
@@ -10,3 +10,5 @@ BEGIN
   DELETE FROM documents          WHERE profile_id = pid;
   DELETE FROM calculation_profiles WHERE id = pid;
 END $$;
+
+SELECT * FROM transaction_rules WHERE profile_id LIKE "dfb96bfb-30e2-492d-85ec-e6747568655d";
